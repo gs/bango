f4cb8491b42b6b83b5f2e8cbc60ce234cb720455
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2011-12-27 12:35:53 +0100

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bango::Application.config.secret_token = '8a7c87fff4d50b7592e7923d247cee60f0347330c4c5870198e1ed98607a04961d9f5ca212e8ab49114a75548f6d9eb99270815d08d4bf7fdb6a5bb3b53ebfe7'
