afa1db54e5922fa430fe84013e83fd1e7993776b
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2011-12-28 19:48:10 +0100

    removed index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     base_title = "Bango"
     @title.nil? ? base_title : "#{base_title} | #{@title}"
   end
+
+  def logo
+    image_tag("logo.png", :alt => "Bango", :class => "round")
+  end
+
 end
