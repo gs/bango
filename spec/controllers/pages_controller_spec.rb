af578623381b6415e51880d305670a4b176f3e9f
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2011-12-28 19:47:48 +0100

    added layouts and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -50,4 +50,17 @@ describe PagesController do
 
   end
 
+  describe "GET 'help' " do
+    it  "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@base_title} | Help")
+    end
+
+  end
+
 end
