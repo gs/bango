15d32aba2b7b231770728dab59061e474d283c7e
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2011-12-29 14:19:54 +0100

    added encrypted password for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regexp = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
   validates :name, :presence => true,
@@ -18,5 +20,9 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regexp, :on => :create },
                     :uniqueness => true
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 
@@ -16,7 +17,10 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @attr = { :name => "Adam", :email => "adam@o2.pl" }
+    @attr = { :name => "Adam",
+              :email => "adam@o2.pl",
+              :password => "secret",
+              :password_confirmation => 'secret' }
   end
   it "should create user with valid data" do
     User.create!(@attr)
@@ -65,6 +69,51 @@ describe User do
     up_email = @attr[:email].upcase
     u = User.new(@attr.merge(:email => up_email))
     u.should_not be_valid
+  end
+
+  describe "passwords" do
+
+    before(:each) do
+      @user = User.new(@attr)
+    end
+
+    it "should have password attribute" do
+      @user.should respond_to(:password)
+    end
+
+    it "should have password confirmation" do
+      @user.should respond_to(:password_confirmation)
+    end
+  end
+
+  describe "password validation" do
+    it "should require password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+    it "should require matching password_confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create(@attr)
+    end
+
+    it "should have an encryptied password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
   end
 end
 
